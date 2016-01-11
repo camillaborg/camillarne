be1bb0e47febd4430e9ecaed2acf8eb75adcac96
Mikaela Grundin <me@mikaelagrundin.com>
2016-01-11 15:15:16 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream'),
+    browserSync = require('browser-sync').create();
+
+
+gulp.task('sass', function () {
+  return sass('app/style/style.scss')
+    .on('error', sass.logError)
+    .pipe(gulp.dest('app/style'));
+});
+
+
+gulp.task('serve', function(){
+    browserSync.init({
+        server: "./app"
+    });
+    
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+    
+    gulp.watch("app/style/style.scss", ['sass']);
+    gulp.watch("app/style/*.css").on('change', browserSync.reload);
+    
+    gulp.watch("app/scripts/*.js").on('change', browserSync.reload);
+});
