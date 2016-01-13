467110c3678ff72bf224e1a88ff6d5dcf5094093
Camilla Borg <camillaborg88@gmail.com>
2016-01-13 16:38:10 +0100

    Question and option-buttons

--- app/scripts/controllers/game-controller.js
+++ app/scripts/controllers/game-controller.js
@@ -1,5 +1,4 @@
-angular.module('app', [])
-    .controller('GameController', GameController);
+app.controller('GameController', GameController);
 
 function GameController($scope){
     $scope.gameID = 'A743Hd';
