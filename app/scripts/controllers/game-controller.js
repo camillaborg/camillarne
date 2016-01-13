f09ea6dc03ddc5979b8e5c8f2b359c0a5d051942
Camilla Borg <camillaborg88@gmail.com>
2016-01-13 14:45:06 +0100

    started GameController

---
+++ app/scripts/controllers/game-controller.js
@@ -0,0 +1,21 @@
+angular.module('app', [])
+    .controller('GameController', GameController);
+
+function GameController($scope){
+    $scope.gameID = 'A743Hd';
+
+    $scope.questions = [
+        {
+          question: "What's your favorite animal?",
+          options: ["Dog", "Monkey", "Cat", "Horse"]
+        },
+        {
+           question: "What's your favorite food?",
+           options: ["Pizza", "Pasta", "Pancakes", "Pony"]
+        },
+
+    ]
+}
+
+
+
