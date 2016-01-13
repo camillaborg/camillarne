8401ed9be1410cf6cc3aa990c62c988446e403ba
Mikaela Grundin <me@mikaelagrundin.com>
2016-01-13 15:09:33 +0100

    Bower, Firebase initialize

--- app/scripts/controllers/main-controller.js
+++ app/scripts/controllers/main-controller.js
@@ -1,7 +1,21 @@
-angular.module('app', [])
-    .controller('MainController', MainController);
+app.controller('MainController', MainController);
 
-function MainController(){
-    var vm = this;
-    vm.gameID = 'A743Hd';
+function MainController($scope, CurrentGame, GameID, FirebaseURL, $firebaseObject, $firebaseArray, $firebaseAuth){
+    $scope.gameID = GameID;
+    var ref = new Firebase(FirebaseURL),
+        userRef,
+        authObj = $firebaseAuth(ref);
+ /*       
+   authObj.$authAnonymously().then(function(authData) {
+      console.log("Logged in as:", authData.uid);
+      userRef = new Firebase(FirebaseURL + 'presence/' + authData.uid);
+       ref.child(GameID).child('players').child(authData.uid).set({name: 'Emma', score: 0});
+        userRef.onDisconnect().remove();
+      
+    }, {remember: "sessionOnly"})
+     .catch(function(error) {
+      console.error("Authentication failed:", error);
+    });
+  */
+  
 }
