d03b0f7d90a985d417a8bc2c382916640ce516ab
Camilla Borg <camillaborg88@gmail.com>
2016-01-15 15:17:17 +0100

    Slice function

--- app/scripts/controllers/game-controller.js
+++ app/scripts/controllers/game-controller.js
@@ -11,7 +11,7 @@ function GameController($scope){
         {
            question: "What's your favorite food?",
            options: ["Pizza", "Pasta", "Pancakes", "Pony"]
-        },
+        }
 
     ]
 
@@ -39,6 +39,23 @@ function GameController($scope){
             color: "green"
         }
     ]
+
+    $scope.currentPlayer = $scope.players[0];
+
+    // Funktion för att slice:a ut your till currentPlayer
+    function replaceWithName (question){
+        var slice = question.replace("your", $scope.currentPlayer.name + "'s");
+        return slice;
+    }
+
+    // currentQuestion formateras och körs med slice-funktionen
+    $scope.currentQuestion = $scope.questions[0];
+    $scope.currentQuestion.formattedQuestion = replaceWithName($scope.currentQuestion.question);
+
+    /* Loggar
+    console.log($scope.currentQuestion.formattedQuestion);
+    console.log(replaceWithName($scope.questions[0].question));
+    */
 }
 
 
