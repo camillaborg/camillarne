c2663cf0d695c8eab7a6dda5132783114f806975
Emma Berglund <emma.berglund@live.se>
2016-01-22 14:55:33 +0100

    avatars (not fully working)

--- app/scripts/services/current-user.js
+++ app/scripts/services/current-user.js
@@ -3,10 +3,10 @@ app.service('CurrentUser', CurrentUser);
 function CurrentUser(UserState, Game, FirebaseRef, $firebaseAuth){
     var self = this,
         colors = ['pink', 'green', 'blue', 'red', 'orange'],
-        avatars = ['fish'],
+        avatars = ['fish', 'snail', 'bird', 'cat'],
         color = colors[Math.floor(Math.random() * colors.length)],
         avatar = avatars[Math.floor(Math.random() * avatars.length)];
-  
+
 
     function setDetails(name, id){
         var user = {};
@@ -27,7 +27,7 @@ function CurrentUser(UserState, Game, FirebaseRef, $firebaseAuth){
         this.ready = status;
         this.ref.update({ready: status});
     }
-    
+
     this.setUser = function(name){
         $firebaseAuth(FirebaseRef).$authAnonymously().then(function(authData) {
             var user = setDetails(name, authData.uid);
@@ -43,7 +43,7 @@ function CurrentUser(UserState, Game, FirebaseRef, $firebaseAuth){
           console.error("Authentication failed:", error);
         });
     }
-    
+
     this.chooseAnswer = function(answer){
         self.answer = answer;
         self.ref.update({answer: answer});
